64e5d24bd72518e1d56860e87e7e7476b16f5fae
Sidi Mohammed Taleb <talebsms@hotmail.fr>
2023-03-09 09:05:57 +0100

    Remplacer un élement

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ elt.innerHTML = "<ul><li>Element 1</li><li>Element 2</li><li>Element 3</li></ul>
 //document.getElementsByClassName()
 //document.getElementsByTagName()
 
+//Ajout d'un élement. 
 const newElt = document.createElement("div");
 
 elt.appendChild(newElt);
+
+// Supprimer et remplacer des élements.
+// elt.removeChild(newElt); // Supprime l'élément newElt de l'élément elt.
+elt.replaceChild(document.createElement("article"), newElt); // Remplace l'éléement newElt par un nouvel élement de type article
+
