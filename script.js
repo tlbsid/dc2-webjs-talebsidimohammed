8723ac44e39a63a7c5486733aba9cd83f608ecbd
Sidi Mohammed Taleb <talebsms@hotmail.fr>
2023-03-09 11:36:42 +0100

    Suite des consignes créer un local storage et afficher un des attribut

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ elt.appendChild(newElm);
 newElm.innerHTML = "<span>Je suis la span 1</span> <span>Je suis la span 1</span> <span>Je suis la span 1</span>"
 elt.replaceChild(document.createElement("section"), newElm); // Remplace l'éléement newElt par un nouvel élement de type article
 
-// Evenement.
+// Evenement. // La ligne suivante est une fonctione mise en commentaire
+// pour permettre à la fonction d'aprés de fonctionner.
 /* elt.addEventListener('click', function(){
     elt.innerHTML ="C'est Cliqué !";
 }); */
@@ -28,3 +29,29 @@ function changerCouleur() {
 
 // Ajout d'un écouteur d'événement sur la troisième div
 div3.addEventListener("click", changerCouleur);
+
+// Créer un objet serpière
+const serpiere = {
+    couleur: "bleu",
+    matiere: "microfibre",
+    taille: "moyenne"
+  };
+  
+  // Enregistrer l'objet serpière dans LocalStorage
+  localStorage.setItem("serpiere", JSON.stringify(serpiere));
+  
+  // Récupérer le contenu de LocalStorage
+  const serpiereLocalStorage = JSON.parse(localStorage.getItem("serpiere"));
+  
+  // Afficher le contenu de l'objet serpière en format JSON dans la console du développement
+  console.log(serpiereLocalStorage);
+
+  //Afficher un des attributs qui est présent dans la serpières dans une des
+  // div qu'on a sur le code d'avant
+
+// Accéder à l'attribut couleur de l'objet serpière
+const couleurSerpiere = serpiereLocalStorage.couleur;
+
+// Afficher la couleur de la serpière dans la deuxième div
+const div2 = elt.getElementsByTagName("div")[1];
+div2.innerHTML = "La couleur de la serpière est " + couleurSerpiere;
