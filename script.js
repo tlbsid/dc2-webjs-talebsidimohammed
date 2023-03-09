3b650585badd1ec672e28bb2314458037e2e559f
Sidi Mohammed Taleb <talebsms@hotmail.fr>
2023-03-09 08:43:35 +0100

    Ajout de la fonction Event Listener

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ elt.innerHTML = "<div>tytytry</div>";
 
 //document.getElementsByClassName()
 //document.getElementsByTagName()
+
+const newElt = document.createElement("div");
+let elt = document.getElementById("main");
+
+elt.appendChild(newElt);
+
+const elt = document.getElementById('mon-lien');
+elt.addEventListener('click', fucntion() {
+    elt.innerHTML"C'est cliqué";
+});
