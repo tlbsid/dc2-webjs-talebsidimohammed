29d08fd0c6679a62b42a4cee8c7b0b8a3be060c0
Sidi Mohammed Taleb <talebsms@hotmail.fr>
2023-03-09 09:19:30 +0100

    Modification de consignes et mise à jour des Div et Section

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 let elt = document.getElementById('test1');
-elt.innerHTML = "<ul><li>Element 1</li><li>Element 2</li><li>Element 3</li></ul>";
+elt.innerHTML = "<div>Je suis la div 1</div> <div>Je suis la div 2</div> <div>Je suis la div 3</div>";
 
+//Ajout d'un élement. 
+var newElm = document.createElement('div');
+newElm.innerHTML = "<div>Je suis la div 4</div ><div>Je suis la div 5</div> <div>Je suis la div 6</div>"
+elt.appendChild(newElm);
 //document.getElementsByClassName()
 //document.getElementsByTagName()
 
-//Ajout d'un élement. 
-const newElt = document.createElement("div");
-
-elt.appendChild(newElt);
-
 // Supprimer et remplacer des élements.
 // elt.removeChild(newElt); // Supprime l'élément newElt de l'élément elt.
-elt.replaceChild(document.createElement("article"), newElt); // Remplace l'éléement newElt par un nouvel élement de type article
+newElm.innerHTML = "<span>Je suis la span 1</span> <span>Je suis la span 1</span> <span>Je suis la span 1</span>"
+elt.replaceChild(document.createElement("section"), newElm); // Remplace l'éléement newElt par un nouvel élement de type article
 
 // Evenement.
 elt.addEventListener('click', function(){
     elt.innerHTML ="C'est Cliqué !";
 });
+
