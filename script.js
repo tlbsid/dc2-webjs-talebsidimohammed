88cb336e22e05da48a4ed7b150848dc10ba9efc0
Sidi Mohammed Taleb <talebsms@hotmail.fr>
2023-03-09 09:47:20 +0100

    Changer la couleur du premier élement en cliquant sur le 3eme éléement

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let elt = document.getElementById('test1');
-elt.innerHTML = "<div>Je suis la div 1</div> <div>Je suis la div 2</div> <div>Je suis la div 3</div>";
+elt.innerHTML = "<div >Je suis la div 1</div> <div>Je suis la div 2</div> <div>Je suis la div 3</div>";
 
 //Ajout d'un élement. 
 var newElm = document.createElement('div');
@@ -14,7 +14,17 @@ newElm.innerHTML = "<span>Je suis la span 1</span> <span>Je suis la span 1</span
 elt.replaceChild(document.createElement("section"), newElm); // Remplace l'éléement newElt par un nouvel élement de type article
 
 // Evenement.
-elt.addEventListener('click', function(){
+/* elt.addEventListener('click', function(){
     elt.innerHTML ="C'est Cliqué !";
-});
+}); */
 
+const div1 = elt.getElementsByTagName("div")[0];
+const div3 = elt.getElementsByTagName("div")[2];
+
+// Fonction de changement de couleur de la première div
+function changerCouleur() {
+  div1.style.color = "red";
+}
+
+// Ajout d'un écouteur d'événement sur la troisième div
+div3.addEventListener("click", changerCouleur);
